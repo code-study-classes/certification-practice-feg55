97f2fea3155abf946248c86f7f296d0b5444950e
feg55 <andhkj@gmail.com>
2025-06-03 14:39:55 +0300

    3 commit

--- strings.js
+++ strings.js
@@ -11,15 +11,15 @@ const countUppercaseLetters = (str) => {
 console.log(countUppercaseLetters("DDDdd"))
 
 const combineStrings = (num1, num2, str1, str2) => {
-    // let arr1 = str1.split('')
-    // let arr2 = str2.split('')
+    let s1 = ''
+    let s2 = ''
     let arr = []
-    for (let i = 0; i < num1; i++) {
-        arr.push(str1[i])
+    for (let i = 0; i < num1 && str1.length; i++) {
+        s1 += str1[i]
     }
-    for (let i = str2.length - 1; str2.length - num2 >= 0; i--) {
-        arr.push(str2[i])
+    for (let i = str2.length - num2; i < str2.length; i++) {
+        s2 += str2[i]
     }
-    return arr.join('')
+    return s1 + s2
 }
-console.log(combineStrings(3, 2, 'Hello', 'World'))
+console.log(combineStrings(1, 3, 'Apple', 'Banana'))
